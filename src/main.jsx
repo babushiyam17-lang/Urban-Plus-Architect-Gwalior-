db1dbd56b8d6261826040d5b2d8102f0102d913e
babushiyam17-lang <babushiyam17@gmail.com>
2026-08-17 17:40:56 +0530

    Load 3D project visual styling

--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,6 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './professional-theme.css'
 import './video-hero.css'
+import './project-3d.css'
 
 createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
